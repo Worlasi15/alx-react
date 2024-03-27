7502e0bfd99ea57c949007aa84db6c05b9fc9346
Worlasi15 <efyawalasi@gmail.com>
2024-03-27 10:19:19 -0700

    get immutable object

--- 0x06-React_Immutable/0-fromjs.js
+++ 0x06-React_Immutable/0-fromjs.js
@@ -1,7 +1,7 @@
 const { Map } = require('immutable');
 
-module.exports = getImmutableObject;
-
 function getImmutableObject(object) {
   return Map(object);
 }
+
+module.exports = getImmutableObject;
